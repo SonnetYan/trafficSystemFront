4c042f0f8edc78eb849b8ea4246d202d72b370eb
SonnetYan <yanmingxi7@gmail.com>
2023-09-01 11:57:43 -0400

    logic of queue

--- src/components/TrafficAnimation.vue
+++ src/components/TrafficAnimation.vue
@@ -1,106 +1,96 @@
 <template>
     <div>
+        <label for="speed-slider">Adjust Time Speed:</label>
+        <input id="speed-slider" type="range" v-model="timeSpeed" min="1" max="10" />
         <svg width="1000" height="600"></svg>
+        <div class="timer">Current Time: {{ typeof currentTime === 'number' ? currentTime.toFixed(2) : '0.00' }}</div>
+        <div class="objective-timer">Objective Time: {{ isNaN(objectiveTime) ? '0.00' : objectiveTime.toFixed(2) }}</div>
     </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
+import sorted_data from '../data/sorted_data.json';
 
 export default {
-  name: 'TrafficAnimation',
+    name: 'TrafficAnimation',
     data() {
         return {
             svg: null,
-            width: 600,
+            width: 1000,
             height: 600,
-            trafficRoad1: null,
-            trafficRoad2: null,
-            eastSideQueue:null,
-            westSideQueue:null,
-            block: null,
-            car1: null,
-            car2: null,
-            blockWidth: 50,
-            trafficRoadWidth:600,
-            roadStart: 20, // Road start position
-            roadEnd: 620, // Road end position
-
-            roadColor: d3.rgb(170, 238, 207),
-            road2Color:d3.color("#069e6bec")
+            carRadius: 10,
+            carData: [],
+            queueStart: 200, 
+            queueEnd: 800,
+            currentTime: 0,
+            objectiveTime: 0,
+            timeSpeed: 5,
+            lastRender: null,
+            carsInQueue: 0
         }
     },
     mounted() {
+        this.carData = sorted_data.map(d => ({...d, processed: false}));
         this.svg = d3.select("svg");
         this.width = +this.svg.attr("width");
         this.height = +this.svg.attr("height");
 
-        this.trafficRoad1 = this.svg.append("rect")
-            .attr("x", this.roadStart)
-            .attr("y", this.height / 2 - 10)
-            .attr("width", this.trafficRoadWidth)
-            .attr("height", 20)
-            .attr("class", "road")
-            .attr("fill", this.roadColor);
-
-        this.trafficRoad2 = this.svg.append("rect")
-            .attr("x", this.roadStart)
-            .attr("y", this.height / 2 + 10)
-            .attr("width", this.trafficRoadWidth)
-            .attr("height", 20)
-            .attr("class", "road")
-            .attr("fill", this.road2Color);
-
-        this.block = this.svg.append("rect")
-            .attr("x", (this.trafficRoadWidth - this.blockWidth) / 2)
-            .attr("y", this.height / 2 - 10)
-            .attr("width", this.blockWidth)
-            .attr("height", 20)
-            .attr("class", "block");
-
-        this.car1 = this.svg.append("circle")
-            .attr("r", 10)
-            .attr("cx", this.roadStart)
-            .attr("cy", this.height / 2 - 0)
-            .attr("fill", "red");
-
-        this.car2 = this.svg.append("circle")
-            .attr("r", 10)
-            .attr("cx", this.roadEnd)
-            .attr("cy", this.trafficRoadWidth / 2 + 20)
-            .attr("fill", "blue");
+        this.svg.append("rect")
+            .attr("x", this.queueStart)
+            .attr("y", this.height / 2 - this.carRadius)
+            .attr("width", this.queueEnd - this.queueStart)
+            .attr("height", this.carRadius * 2)
+            .attr("fill", "#e0e0e0")
+            .attr("stroke", "black");
 
         this.animate();
     },
+    beforeUnmount() {
+        cancelAnimationFrame(this.animationFrame);
+    },
     methods: {
-      animate() {
-    this.car1.transition()
-        .duration(5000)
-        .ease(d3.easeLinear)
-        .attr("cx", this.roadEnd);
+        animate(timestamp = 0) {
+            if (this.lastRender === null) {
+                this.lastRender = timestamp;
+                this.animationFrame = requestAnimationFrame(this.animate);
+                return;
+            }
+            
+            const delta = (timestamp - this.lastRender) / 1000;  
+            this.lastRender = timestamp;
 
-    this.car2.transition()
-        .duration(5000)
-        .ease(d3.easeLinear)
-        .attr("cx", this.roadStart);
-}
+            this.objectiveTime += delta * this.timeSpeed;
 
+            this.carData.forEach((d) => {
+                if (!d.processed && this.objectiveTime >= d.eventArrivingTime) {
+                    d.processed = true;
+                    this.currentTime = parseFloat(d.eventArrivingTime);
+                    this.carsInQueue++;
+
+                    const car = this.svg.append("circle")
+                        .attr("cx", this.queueStart)
+                        .attr("cy", this.height / 2)
+                        .attr("r", this.carRadius)
+                        .attr("fill", "blue");
+                    
+                    const finalPosition = this.queueEnd - (this.carsInQueue * 2 * this.carRadius)+ this.carRadius;
+
+                    car.transition()
+                        .duration((finalPosition - this.queueStart) / this.timeSpeed * 10)
+                        .attr("cx", finalPosition);
+                }
+            });
+
+            this.animationFrame = requestAnimationFrame(this.animate);
+        }
     }
 }
 </script>
 
 <style scoped>
-.road {
-    fill: rgb(170, 238, 207);
-    stroke:rgb(29, 1, 1);
-    stroke-width: 2;
-}
-
-.block {
-    fill: #069e6bec;
+.timer, .objective-timer {
+    font-size: 20px;
+    margin-top: 10px;
 }
 </style>
-
-
-
-
